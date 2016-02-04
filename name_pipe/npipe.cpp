aebb8d557aa1f46127e5643e85f5b5b96213d396
Ivan Plotnikov <ivan_plotnikov@senta.pro>
2016-02-04 22:54:38 +0300

    task - name pipe | add npipe.cpp

--- name_pipe/npipe.cpp
+++ name_pipe/npipe.cpp
@@ -1,1 +1,40 @@
-#include <stdio>
+/*
+ * Задача на именованные каналы.
+ * Напишите программу, которая создает два именованных канала - /home/box/in.fifo и /home/box/out.fifo
+ * Пусть программа читает in.fifo и все прочитанное записывает в out.fifo.
+ *
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+    pid_t ppid = getpid();
+    printf("Parent PID = %i\n", ppid);
+
+    int in_fifo  = mkfifo("in.fifo", 0666);
+    int out_fifo = mkfifo("out.fifo", 0666);
+
+    const unsigned int size_bf = 512;
+    char bf[size_bf];
+
+    while ( int sz = read(in_fifo, bf, size_bf) ) {
+        printf("Read: %s\n", bf);
+        write( out_fifo, bf, sz );
+
+        memset(bf, 0, 512);
+    }
+
+
+    close( in_fifo );
+    close( out_fifo );
+
+    return 0;
+}
